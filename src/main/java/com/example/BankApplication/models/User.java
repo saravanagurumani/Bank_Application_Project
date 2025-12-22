55cc7e5e2d1b36bcf001dcb0b842e1417fd614c0
lm9447210-dev <lm9447210@gmail.com>
2025-12-22 11:32:28 +0530

    feat:User class will be created

---
+++ src/main/java/com/example/BankApplication/models/User.java
@@ -0,0 +1,16 @@
+package com.example.BankApplication.models;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+
+}

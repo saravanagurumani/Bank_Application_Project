fcaaad36f34b926a8a13158466d5c81461229ccc
saravanagurumani <itssaravana2001@gmail.com>
2025-12-22 13:03:51 +0530

    feat:project structure added

--- src/main/java/com/example/BankApplication/service/AccountService.java
+++
@@ -1,4 +0,0 @@
-package com.example.BankApplication.service;
-
-public class AccountService implements AccountServiceInter{
-}
--- src/main/java/com/example/BankApplication/service/AccountServiceInter.java
+++
@@ -1,4 +0,0 @@
-package com.example.BankApplication.service;
-
-public interface AccountServiceInter {
-}
